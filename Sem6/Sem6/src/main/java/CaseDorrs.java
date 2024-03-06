00a1da1c286a972bfce294eb7f2b5f49bf8d551b
Mipusatina <mipspam2@mail.ru>
2024-03-06 15:35:47 +0100

    Sem 6. DZ. com. 2

---
+++ Sem6/Sem6/src/main/java/CaseDorrs.java
@@ -0,0 +1,43 @@
+//20240306 mip24
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class CaseDorrs {
+    private boolean doors[];
+
+    public CaseDorrs(){
+        this(3);
+    }
+
+    public CaseDorrs(int number){
+        Random rnd = new Random();
+        doors = new boolean[number];
+        int gluckDoor = rnd.nextInt(1000)%number;
+        doors[gluckDoor] = true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("Case: ");
+        for(int i = 0; i < doors.length; i++){
+            result.append(String.format("door %d: %s, ", i, (doors[i]?"CAR":" - ")));
+        }
+        return result.substring(0,result.length()-2).toString();
+    }
+
+    public int getDoorWithNothing(int currentDoor){
+        //Список все пустых дверей, за исключением выбранной
+        List<Integer> doorsWithNothing = new ArrayList<>();
+        for(int i = 0; i < doors.length; i++){
+            if(doors[i] == false && i != currentDoor){
+                doorsWithNothing.add(i);
+            }
+        }
+        //Рандомно выдаем одну из дверей.
+        Random rnd = new Random();
+        int door = doorsWithNothing.get(rnd.nextInt(1000)%doorsWithNothing.size());
+        return door;
+    }
+
+}
--- Sem6/Sem6/src/main/java/Main.java
+++ Sem6/Sem6/src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi");
+        int maxTry = 1000;
+        int countOfDoor = 3;
+
+        for(int i = 0; i < maxTry; i++){
+            CaseDorrs caseDoors = new CaseDorrs(countOfDoor);
+                System.out.println(i + " " + caseDoors);
+        }
     }
 }
